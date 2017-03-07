5be6b121fd7aab77fe300e317757e12ba055933d
trouse546 <tdrouse5@gmail.com>
2017-03-06 19:55:23 -0500

    Create RemovePlan.php

---
+++ RemovePlan.php
@@ -0,0 +1,24 @@
+<?php
+
+function RemovePlan($id) {
+$servername = "localhost"; 
+$username = "root"; 
+$password = "root"; 
+$dbname = "CPET";
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+// Check connection 
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+$sql = "DELETE FROM Plans WHERE plan_id = '". $id['plan_id']."' and semester = '". $id['semester'] ."' and c_id = '". $id['c_id'] ."'";
+
+if ($conn->query($sql) == TRUE)
+{ echo "Record removed successfully"; } 
+else 
+{ echo "Error removing record: " . $conn->error; }
+
+$conn->close(); 
+}
+$arr = array('plan_id' => '3', 'semester' => 'Spr18', 'c_id' => 'CPET107');
+
+RemovePlan($arr);
+?>
