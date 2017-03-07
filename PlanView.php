be8b42671da5a25778956fd20d3f1ff2a874f795
trouse546 <tdrouse5@gmail.com>
2017-03-06 20:15:55 -0500

    Create PlanView.php

---
+++ PlanView.php
@@ -0,0 +1,28 @@
+<?php
+function PlansView($id) {
+$servername = "localhost"; 
+$username = "root"; 
+$password = "root"; 
+$dbname = "CPET";
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+// Check connection 
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+$sql = "SELECT P.semester, c_name, credits FROM Plans P JOIN Courses C ON P.c_id = C.c_id WHERE plan_id = '". $id['plan_id'] . "'and P.semester = '". $id['semester'] ."'";
+$result = $conn->query($sql);
+$return_arr = array();
+if($result->num_rows > 0)
+{
+while($row = $result->fetch_assoc())
+{
+$row_array['semester'] = $row['semester'];
+$row_array['c_name'] = $row['c_name'];
+$row_array['credits'] = $row['credits'];
+array_push($return_arr,$row_array);
+}
+}
+echo json_encode($return_arr);
+$conn->close(); 
+}
+Plans($_POST['plan']);
+?>
